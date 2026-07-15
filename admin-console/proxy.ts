460a31be6f122bd5dd3a6f94a536657a1160328f
YawAsanteBoadu <yawasanteboadu@gmail.com>
2026-07-15 07:58:40 +0000

    fix: fix logo to render on admin login page

--- admin-console/proxy.ts
+++ admin-console/proxy.ts
@@ -20,5 +20,5 @@ export function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next|api|favicon.ico).*)'],
+  matcher: ['/((?!_next|api|favicon.ico|AbProperties_logo.png).*)'],
 };
