ea4242982121b640d2fb2c6b8da57bda460fe867
YawAsanteBoadu <yawasanteboadu@gmail.com>
2026-07-16 02:19:49 +0000

    upgrade: improve logo sharpness accross all UI

--- web/components/shared/Navbar/Navbar.tsx
+++ web/components/shared/Navbar/Navbar.tsx
@@ -35,7 +35,7 @@ export default function Navbar() {
               src="/AbProperties_logo.png"
               alt={`${BRAND.name} Logo`}
               fill
-              sizes="(max-width: 768px) 65%, auto"
+              sizes="(max-width: 768px) 124px, 150px"
               priority
               className={styles.logoImage}
             />
